1db5b7deff17e1d97eace8bc10c93ad02df742d9
Eko Prastyo <604912+temon@users.noreply.github.com>
2024-03-05 00:55:38 +1100

    connect to the neonDb

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 const serverless = require("serverless-http");
 const express = require("express");
+const { neon } = require("@neondatabase/serverless");
 const app = express();
 
-app.get("/", (req, res, next) => {
+async function dbClient(){
+  return neon(process.env.DATABASE_URL);
+}
+
+async function indexRoute(req, res, next){
+  let db = await dbClient()
+  const [result] = await db`select now();`
   return res.status(200).json({
     message: "Hello from root!",
+    nowData: result.now
   });
-});
+
+}
+
+app.get("/", indexRoute);
 
 app.get("/path", (req, res, next) => {
   return res.status(200).json({
